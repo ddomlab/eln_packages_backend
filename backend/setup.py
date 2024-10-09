81ba5fe52172aa65bb5957e80815dd0196d0633c
12buntu <kylemhollars@gmail.com>
2024-10-09 00:52:54 -0400

    fixed setup.py files

--- backend/setup.py
+++ backend/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Ddomlab-ELN-Automation-Backend",
+    name="ddomlab_eln_packages_backend",
     version="0.1.0",
     packages=find_packages(),
     author="Kyle Hollars",
