d9a5fbb6221413b65e963a6f2965eb8847e1adac
12buntu <kylemhollars@gmail.com>
2024-10-09 00:17:06 -0400

    removed a comment that is no longer relevant

--- backend/autofill.py
+++ backend/autofill.py
@@ -7,7 +7,6 @@ import json
 rm = Resource_Manager()
 
 
-# a somewhat unnecessary wrapper for pubchem.py
 # CAS numbers fall in the 'name' category on pubchem, so they are searched as names
 # you could also search by the common name or any other synonym, however CAS numbers
 # should return more consistent results
