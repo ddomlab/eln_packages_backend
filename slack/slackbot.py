f54b2fc670a2c86834a4fe76430447a2098371d5
12buntu <kylemhollars@gmail.com>
2024-10-22 16:49:09 -0400

    forgot to stage some changes in last commit.

--- slack/slackbot.py
+++ slack/slackbot.py
@@ -1,14 +1,19 @@
 import requests
 import json
+# channel ID of the eln_bot channel, can be found by clicking "view channel details"
+DEFAULT_CHANNEL: str = 'C0784B6DH45'
 # headers and authentication token
-headers = {
-    'Authorization' : json.loads(open('/slack/token.json').read())['slack_bot_token'],
+headers: dict = {
+    'Authorization' : 'Bearer ' + json.loads(open('slack/slack_info.json').read())['slack_bot_token'],
     'Content-Type': 'application/json'
 }
-# channel id of the channel where the bot will post
-json_data = {
-    'channel' : 'C07SSMMU9E1',
-    'text' : 'hello world'
-}
 
-response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, json=json_data)
+json_data: dict = {
+    'channel' : "",
+    'text' : ''
+}
+# Very simple bot. Sends a message in its designated channel when called. 
+def send_message(message: str, channel: str = DEFAULT_CHANNEL):
+    json_data['text'] = message # set the text field to the given message
+    json_data['channel'] = channel # set the channel field to the given channel, default channel for this bot is eln_bot
+    response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, json=json_data)
