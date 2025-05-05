dbdd83e78831a0cd7d284d1f9da741af01b697cd
12buntu <kylemhollars@gmail.com>
2025-05-05 12:42:45 -0400

    fixed datetime

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import slack.slackbot as slackbot
 from datetime import date
 from check_peroxides import check_all_classes
 
-today = date.today.strftime("%m-%d")
+today = date.today().strftime("%m-%d")
 try:
     autofill.autofill(300, info=True, label=True, image=True, size=5)
 except Exception as e: 
